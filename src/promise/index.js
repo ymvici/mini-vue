8b93f76440943fa0c90636288263b5505169f447
vici_y <vici_y@163.com>
2022-06-14 10:24:03 +0800

    feat: 封装then函数

--- src/promise/index.js
+++ src/promise/index.js
@@ -2,7 +2,7 @@
  * @Author: vici_y vici_y@163.com
  * @Date: 2022-06-05 17:22:03
  * @LastEditors: vici_y vici_y@163.com
- * @LastEditTime: 2022-06-14 10:13:31
+ * @LastEditTime: 2022-06-14 10:23:45
  * @FilePath: \mini-vue\src\promise\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -37,5 +37,42 @@ class HD {
       this.status = HD.REJECTED;
       this.value = reason;
     }
+  }
+  then(onFulfilled, onRejected) {
+    // 当不传resolve或者reject函数时，会自动封装成函数
+    if (typeof onFulfilled != "function") {
+      onFulfilled = () => {};
+      // onFulfilled = () => this.value;
+    }
+    if (typeof onRejected != "function") {
+      onRejected = () => {};
+      // onRejected = () => this.value;
+    }
+    let promise = new HD((resolve, reject) => {
+      // 当状态改变后才会执行对应函数
+      // if (this.status == HD.PENDING) {
+      //   this.callbacks.push({
+      //     onFulfilled: (value) => {
+      //       this.parse(promise, onFulfilled(value), resolve, reject);
+      //     },
+      //     onRejected: (value) => {
+      //       this.parse(promise, onRejected(value), resolve, reject);
+      //     },
+      //   });
+      // }
+      if (this.status == HD.FULFILLED) {
+        onFulfilled(this.value);
+        // setTimeout(() => {
+        //   this.parse(promise, onFulfilled(this.value), resolve, reject);
+        // });
+      }
+      if (this.status == HD.REJECTED) {
+        onRejected(this.value);
+        // setTimeout(() => {
+        //   this.parse(promise, onRejected(this.value), resolve, reject);
+        // });
+      }
+    });
+    return promise;
   }
 }
